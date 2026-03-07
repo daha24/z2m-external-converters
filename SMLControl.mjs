c5c87b59a7b643f53a85f5040ef88936d942af59
daha24 <david.hackajlo@gmail.com>
2026-03-07 23:27:04 +0100

    sml control +refac to new wincover version

--- SMLControl.mjs
+++ SMLControl.mjs
@@ -1,84 +1,30 @@
 import * as m from "zigbee-herdsman-converters/lib/modernExtend";
-import * as exposes from "zigbee-herdsman-converters/lib/exposes";
-import * as reporting from "zigbee-herdsman-converters/lib/reporting";
+import { readGenBasic } from "./azigbee/config.mjs";
+import WinCover from "/config/zigbee2mqtt/external_converters/azigbee/wincover.mjs";
 
-const e = exposes.presets;
-const ea = exposes.access;
+const endpoints = { roller: 1 };
+const keys = [
+  "activity",
+  "controls",
+  "calibration_mode",
+  "calibration_time",
+  "motor_reversal",
+  "identify"
+];
 
 export default {
   zigbeeModel: ["SMLControl"],
   model: "SMLControl",
   vendor: "FUNAMI corp. Ltd.",
-  description: "Velux SML Rolershade Control Device",
+  description: "Velux SML Roller Shutter Control Device",
+  ota: true,
   extend: [
-    m.enumLookup({
-      name: "moving",
-      label: "Moving",
-      description: "",
-      lookup: {
-        IDLE: 0,
-        UP: 1,
-        DOWN: 2,
-      },
-      endpoint: 1,
-      cluster: "closuresWindowCovering",
-      attribute: "tuyaMovingState",
-      access: "STATE_GET",
-    }),
-    m.windowCovering({ controls: ["lift"] }),
-    m.identify(),
-    m.binary({
-      name: "calibration",
-      label: "Calibration",
-      description: "Set calibration ON, move all down, stop, move all up, stop, set calibration OFF. New calibration time is recorded. ",
-      valueOn: ["ON", 1],
-      valueOff: ["OFF", 0],
-      entityCategory: "config",
-      endpoint: 1,
-      cluster: "closuresWindowCovering",
-      attribute: "tuyaCalibration",
-      access: "ALL",
-    }),
-    m.numeric({
-      name: "calibration_time",
-      label: "Calibration time",
-      description: "",
-      unit: "s",
-      valueMin: 0,
-      valueMax: 120,
-      valueStep: 1,
-      entityCategory: "config",
-      endpoint: 1,
-      cluster: "closuresWindowCovering",
-      attribute: "moesCalibrationTime",
-      access: "ALL",
-    }),
-    m.binary({
-      name: "motor_reversal",
-      label: "Motor reversal",
-      description: "",
-      valueOn: ["ON", 1],
-      valueOff: ["OFF", 0],
-      entityCategory: "config",
-      endpoint: 1,
-      cluster: "closuresWindowCovering",
-      attribute: "tuyaMotorReversal",
-      access: "ALL",
-    }),
+    m.deviceEndpoints({ endpoints }),
+    ...WinCover.attributes("roller", endpoints.roller, {}, keys),
   ],
-  meta: {},
-  exposes: [],
+  meta: { multiEndpoint: true },
   configure: async (device, coordinatorEndpoint) => {
-    const endpoint = device.getEndpoint(1);
-    await endpoint.read("genBasic", [
-      "modelId",
-      "swBuildId",
-      "dateCode",
-      "powerSource",
-    ]);
-    await reporting.bind(endpoint, coordinatorEndpoint, [
-      "closuresWindowCovering",
-    ]);
-    await reporting.currentPositionLiftPercentage(endpoint);
+    await readGenBasic(device, endpoints.roller);
+    await WinCover.configure(device, coordinatorEndpoint, endpoints.roller, keys);
   },
 };
